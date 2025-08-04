8ca9cb862adc568fdbf9ca57d471656879db687f
MB9w9 <misuzu.beppu@gmail.com>
2025-08-05 00:36:03 +0900

    noodle.htmlの課題を入力しました

--- js/noodle.js
+++ js/noodle.js
@@ -1,32 +1,23 @@
-// タイマーの識別番号を保存（初期値：-1（停止））
-let timerId = -1;
+let count = 10; //初期値10秒
 
-// ボタンにクリック時のイベント処理を登録
-document.getElementById('btn').addEventListener('click', ()=>{
-    // クリック時、停止状態ならタイマーを実行
-    
+// カウント欄を取得
+const counter = document.getElementById('counter');
+// ボタンを取得
+const btn = document.getElementById('btn');
+// 画像欄を取得
+const img = document.getElementById('img');
 
-    if(timerId === -1){
-        timerId = window.setInterval(()=>{        
-            for(let mm = 10; mm > 0; mm--){    
-                document.getElementById('timer').textContent = `残り：${mm}秒`;
-             }
-        }, 1000);   //1秒ごとに実行
-    }
-
-
-    // -------------------------
-
-    if(timerId === -1){
-        
-        timerId = window.setTimeout(()=>{
-            
-        
-        clearInterval();    //1秒ごとの処理の停止
-        clearTimeout();    //10秒間計測の停止
+// ボタンが押されたときのイベント処理を登録
+btn.addEventListener('click', ()=>{
+    const countdown = setInterval(()=>{
+        if(count > 0){
+            count--;    //残りの秒数をデクリメント
+            counter.textContent = `残り：${count}秒`;
+        }else{
+            clearInterval() // 停止
         }
-        , 10000);  //10秒後に停止する
+    }, 1000);    // 1秒ごとに実行
 
-    }
+    img.src = '/image/daikichi.png'
 
-    
+})
